29396c3605ba1370e9bc994cf8bbf437dbcaace1
Kevin Carmona <110929362+kevin1015carmona@users.noreply.github.com>
2022-10-05 23:07:58 -0500

    Add files via upload

--- events.js
+++ events.js
@@ -44,7 +44,7 @@ window.addEventListener("load", function () {
         animado_2.style.animation = "opacity 1s forwards";
       }
       /*TEC JAVASCRIPT*/
-      if (ubicacion >= 487) {
+      if (ubicacion >= 400) {
         animado_3.style.animation = "opacity 1s forwards";
       }
     });
